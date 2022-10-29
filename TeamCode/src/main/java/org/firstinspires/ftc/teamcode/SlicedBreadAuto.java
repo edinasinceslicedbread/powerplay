fd65f8f6f088fe4949246797fcec8c0d1774c8e6
mark-teskey <mark.teskey@warecorp.com>
2022-10-29 11:27:18 -0500

    Cleanup - Starting lift code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlicedBreadAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlicedBreadAuto.java
@@ -8,17 +8,17 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
-@Autonomous(name="FirstAutoTest", group="Mark")
+@Autonomous(name="FirstAutoTest X", group="Mark")
 public class SlicedBreadAuto extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
-        Pose2d startPose = new Pose2d(0, 0, 0);
+        Pose2d startPose = new Pose2d(36, -64.5, Math.toRadians(90));
 
         drive.setPoseEstimate(startPose);
-        TrajectorySequence trajSeq =
-                drive.trajectorySequenceBuilder(new Pose2d(36, -64.5, Math.toRadians(90)))
+
+        TrajectorySequence trajSeq = drive.trajectorySequenceBuilder(startPose)
                         .lineTo(new Vector2d(36,-36))
                         .strafeTo(new Vector2d(12,-36))
                         .lineTo(new Vector2d(12,-12))
