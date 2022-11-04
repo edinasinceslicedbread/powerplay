ce1121de0e695e25cb8337abd37b8e29df71f4fe
mark-teskey <mark.teskey@warecorp.com>
2022-11-03 19:23:41 -0500

    Added WristCode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WristTool.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WristTool.java
@@ -1,4 +1,42 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 public class WristTool {
+    public Servo wrist;
+    public LinearOpMode opmode;
+    public Telemetry telemetry;
+
+    public void init(HardwareMap hwMap) {
+        wrist=hwMap.get(Servo.class, "wrist");
+    }
+
+    public void moveAbsolute(double target) {
+
+        if (target>1) {
+            target = 1;
+        } else if (target<0) {
+            target = 0;
+        }
+
+        wrist.setPosition(target);
+    }
+
+    public void moveMaxRange() {
+        moveAbsolute(1);
+    }
+
+    public void moveMinRange() {
+        moveAbsolute(0);
+    }
+
+    public void reset() {
+        moveMinRange();
+    }
 }
+
+
