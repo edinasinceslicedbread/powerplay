a34cee352aa4cc81c1194c63abc70f594a8829fc
mark-teskey <mark.teskey@warecorp.com>
2022-12-03 13:34:33 -0600

    Flipped camera orientation to match new build

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlicedBreadAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlicedBreadAuto.java
@@ -124,7 +124,7 @@ public class SlicedBreadAuto extends LinearOpMode {
             @Override
             public void onOpened()
             {
-                camera.startStreaming(1280, 720, OpenCvCameraRotation.UPRIGHT);
+                camera.startStreaming(1280, 720, OpenCvCameraRotation.UPSIDE_DOWN);
             }
 
             @Override
